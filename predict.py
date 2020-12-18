224072d0adddddfd9d951e8d424348702c7cb4e0
Carolyn Duby <cduby@cloudera.com>
2020-12-18 21:14:51 +0000

    if domain contains dots, extract the main domain name

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ import sys
 from src.model import QueryModel
 import logging
 from src.logging import setup_logging
+import tldextract
 
 import pickle
 
@@ -13,6 +14,9 @@ query_model = QueryModel(model_path, "legit")
 def predict(args):
     data = dict(args)
     domain_name = data['domain']
+    if "." in domain_name:
+        domain_parts = tldextract.extract(data['domain'])
+        domain_name = domain_parts.domain
 
     return {
         'legit': query_model.predict(domain_name)
